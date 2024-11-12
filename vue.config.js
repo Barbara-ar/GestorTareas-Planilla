620483dad64e6fd808e3cb0a666f556d540a0e61
Barbara Cabral <cabralba98@mail.com>
2024-11-12 20:54:58 -0300

    Configuración para despliegue en GitHub Pages desde rama personalizacion

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/Vue-Clone/GestorTareas-Planilla/'  // Ruta base correcta para GitHub Pages
+    ? '/GestorTareas-Planilla/'  // Nombre exacto del repositorio
     : '/'
 });
+
